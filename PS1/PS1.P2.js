508625f40113d0806aeed3aa3329f7f901187c39
sherry weC:\Users\Mint\Desktop\BU\CS320\cs320-hw-rafilo\assignments\week2n <wsherry@bu.edu>
2019-05-26 13:56:34 -0400

    Update PS1.P2.js

--- PS1/PS1.P2.js
+++ PS1/PS1.P2.js
@@ -1,27 +1,29 @@
 `use strict`
 //problem 2: do the basic numerical calculation
-function evaluate(str) {
+const getOperator = operator => {
     // change string into integer to do the calculation
-    var left = parseInt(str[0]);
-    var operator = str[1];
-    var right = parseInt(str[2]);
     switch(operator){
         case `+`:
-            return left + right;
+            return (left, right) => left + right;
             break
         case `-`:
-            return left - right;
+            return (left, right) => left - right;
             break
         case `*`:
-            return left * right;
+            return (left, right) => left * right;
             break
         case `/`:
-            return left / right;
+            return (left, right) => left / right;
             break
     }
 } 
-
+const evaluate = str =>{
+    var left = str[0];
+    var operator = str[1];
+    var right = str[2];
+    let mathfunction = getOperator(operator);
+    return mathfunction(left, right);
+}
 const expression = '6-1';
 let operator = evaluate(expression);
-console.log(operator);
 console.log(`${expression} = ${operator}`) 
